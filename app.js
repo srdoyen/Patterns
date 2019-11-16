6e6b3e68ca1eca3bd80b89c44d59b11a6f5e2133
srdoyen <srdoyen@gmail.com>
2019-11-16 12:31:21 -0800

    Module & Revealing Module Pattern

--- app.js
+++ app.js
@@ -1,19 +1,40 @@
-const UICtrl = (function() {
-  let text = "Hello World";
+// const UICtrl = (function() {
+//   let text = "Hello World";
 
-  const changeText = function() {
-    const element = document.querySelector("h1");
-    element.textContent = text;
-  };
+//   const changeText = function() {
+//     const element = document.querySelector("h1");
+//     element.textContent = text;
+//   };
+
+//   return {
+//     callChangeText: function() {
+//       changeText();
+//       console.log(text);
+//     }
+//   };
+// })();
+
+// UICtrl.callChangeText();
+
+// console.log(UICtrl.text);
+const ItemCtrl = (function() {
+  let data = [];
+
+  function add(item) {
+    data.push(item);
+    console.log("Item Added...");
+  }
+
+  function get(id) {
+    return data.find(item => {
+      return item.id === id;
+    });
+  }
 
   return {
-    callChangeText: function() {
-      changeText();
-      console.log(text);
-    }
+    add: add,
+    get: get
   };
 })();
 
-UICtrl.callChangeText();
-
-console.log(UICtrl.text);
+ItemCtrl.add({ id: 1, name: "John" });
